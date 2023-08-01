340a16554f30441d98df1f7307d1fde57e1d4148
Mikko Multimäki <mikko.multimaki@buutti.com>
2023-08-01 12:46:31 +0300

    Simple first connection to the backend.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,44 @@
 import Image from 'next/image'
 
-export default function Home() {
+
+async function getData() {
+  const res = await fetch('https://vialeea-test.azurewebsites.net/api/char/load')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  console.log("Hello!");
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  const data = res.json();
+
+  //console.log(data);
+
+  return data;
+}
+
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
+
+  console.log("Ho!");
+
+  //const numbers = [1, 2, 3, 4, 5];
+  const listItems = data.map((character : any) =>
+    <span>&nbsp;{character.name}</span>
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div id="mydiv" className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex bg-sky-500">
         <p className="hover:bg-sky-700 fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
+          {listItems}
         </p>
         {/*
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
